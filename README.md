5176b321dd6b46771db9e4525254acfb47efea32
Hassan47HS <2019cs617@student.uet.edu.pk>
2022-09-13 21:29:15 +0500

    updated

---
+++ README.md
@@ -0,0 +1,33 @@
+                                        Basic Features 
+
+Features List.
+
+    1. Generate Hash Keys. 
+    2. Convert hash keys into QR Code.
+    3. Upload the Hash keys in to block chain API.
+    4. Return QR to Manufacturer.
+    5. Check validity by scanning QR code.
+
+Possible Features.
+
+    1. A small CMS (Content Management System)
+    2. API for CRM organization.
+    3. IOT Scanner.
+
+Description.
+
+As everyone is aware, there are fake versions of every brand today. Every well-known brand has knockoff producers who sell the same product at lower prices by compromising on quality. Even the original company's experts might not be able to tell the difference between their authentic products and imitations. What if we could identify authentic products using blockchain technology and put a QR code to them as they are being manufactured. The product's QR code will be connected to a Blockchain. The product information and generated QR code for the product can be stored in the database as blocks. Now, individuals may scan the QR code.
+
+Features Description.
+
+    1. Hash Keys and QR Code
+       Details of medicine are given such as name, manufacturing date, etc, on which the hash keys are made and further connected to QR Code and Smart contract.
+
+    2. Hash Keys to Blockchain.
+	Then Hash Keys are stored in the blockchain API.
+
+    3. QR To Manufacturer.
+	After manufacturing the QR it will given to them. They can also keep record of there stuff.
+    
+    4. Checking validity of Product.
+	After Production and assemble of QR on a certain Product the customer will scan the 	product. 
---
+++ client/src/components/Home.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "../css/home.css";
+import pic from '../pic1.jpeg'
+import pic2 from '../pic2.webp'
+import pic3 from '../pic3.jpg'
+import { NavLink } from "react-router-dom";
+
+const Home = ({ account }) => {
+  return (
+    <div className="container">
+      <div id="login-type-container">
+        <h4 style={{ color: "#000", position: "fixed", right: 8, top: 2 }}>
+          Wallet Address:
+          {account.substring(0, 4) +
+            "..." +
+            account.substring(account.length - 4, account.length)}
+        </h4>
+        <br />
+        <div id="login-type">
+          <h1 id="greetings">Welcome to Asset Tracker!</h1>
+          <h1 id="subtitle-txt">
+            A Blockchain Based Fake Product Detection 🕵️‍♀️
+          </h1>
+          <div id="options-container">
+            <NavLink to="/vendor" className="select-link">
+              <div className="options">
+                <img
+                  src={pic}
+                  alt="manufacturer"
+                  className="options-image"
+                />
+                <h1 className="options-image-caption">Manufacturer Login</h1>
+              </div>
+            </NavLink>
+            <NavLink to="/distributorform" className="select-link">
+              <div className="options">
+                <img
+                  src={pic2}
+                  alt="manufacturer"
+                  className="options-image"
+                />
+                <h1 className="options-image-caption">Distributor Login</h1>
+              </div>
+            </NavLink>
+            <NavLink to="/authenticate" className="select-link">
+              <div className="options">
+                <img
+                  src={pic3}
+                  alt="manufacturer"
+                  className="options-image"
+                />
+                <h1 className="options-image-caption">Authenticate Product</h1>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
